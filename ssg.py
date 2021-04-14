ce8b191eaddef1f0eeafb26b3f178e4f0c68f97d
Rahul Sunil <rahulsunil2@gmail.com>
2021-04-15 01:54:17 +0530

    Errors Fixed

--- ssg.py
+++ ssg.py
@@ -2,11 +2,7 @@ import typer
 from ssg.site import Site
 
 def main(source="content", dest="dist"):
-    config = {
-        "source": source,
-        "dest": dest
-    }
-    site = Site(**config)
-    site.build()
+    config = {"source": source, "dest": dest}
+    Site(**config).build()
 
 typer.run(main)
--- ssg/site.py
+++ ssg/site.py
@@ -10,7 +10,7 @@ class Site:
         directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
-        self.dest.mkdir(parents=True, exists_ok=True)
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
