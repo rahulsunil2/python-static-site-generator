e2c3dbebd3e58baebcd98d8209da6d3fe129f355
Rahul Sunil <rahulsunil2@gmail.com>
2021-04-16 01:35:24 +0530

    Module 3

--- ssg/content.py
+++ ssg/content.py
@@ -32,7 +32,7 @@ class Content(Mapping):
         return self.data[key]
 
     def __iter__(self):
-        return self
+        return self.data.__iter__()
 
     def __len__(self):
         return len(self.data)
