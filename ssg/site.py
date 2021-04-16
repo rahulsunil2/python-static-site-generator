f1298ae31e573d9987ee9c65554bd433e2744a6a
Rahul Sunil <rahulsunil2@gmail.com>
2021-04-16 18:35:24 +0530

    Module 4

--- ssg/site.py
+++ ssg/site.py
@@ -29,9 +29,9 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            self.error("No parser for {} extension, file skipped!".format(path.suffix))
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
 
     @staticmethod
     def error(message):
-        sys.stderr.write("\x1b[1;31m{}\n]".format(message))
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
 
